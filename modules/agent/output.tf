768335a561589d6d8267ce4d1b3b937f34998702
Dario Klenke <dario.klenke@heigit.org>
2024-06-18 09:47:01 +0200

    add output extra output to node module to get ip and instances associations

--- modules/agent/output.tf
+++ modules/agent/output.tf
@@ -2,6 +2,10 @@ output "floating_ip" {
   value = module.agent.floating_ip
 }
 
+output "floating_ip_associate" {
+  value = module.agent.floating_ip_associate
+}
+
 output "internal_ip" {
   value = module.agent.internal_ip
 }
